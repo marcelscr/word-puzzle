53a375034eb2f3c369b1f6ec8376e04f58fcf815
Marcel Rebouças <marcel.reboucas@incognia.com>
2022-12-27 09:01:30 -0300

    feat(session): add the session logic

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,48 @@
-import { LoaderFunction } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { ActionFunction, json, LoaderFunction } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
 import _ from 'lodash'
+import moment from 'moment'
 
 import Header from '~/components/header'
 import type { Boss } from '~/data/types'
 import BossesDB from '~/data/database'
+import { commitSession, getSession } from '~/lib/sessions'
+import { getGuesses, incrementGuesses } from '~/data/session'
 
 type Data = {
   boss: Boss
   options: Boss[]
+  guesses: number
 }
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get('Cookie'))
+
   const data: Data = {
     boss: BossesDB.getRandom(),
-    options: BossesDB.getAll()
+    options: BossesDB.getAll(),
+    guesses: getGuesses(session)
   }
 
-  return data
+  return json(data, {
+    headers: {
+      'Set-Cookie': await commitSession(session, {
+        expires: new Date(moment().endOf('day').toString())
+      })
+    }
+  })
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get('Cookie'))
+  incrementGuesses(session)
+
+  return json(
+    {},
+    {
+      headers: { 'Set-Cookie': await commitSession(session) }
+    }
+  )
 }
 
 export default function Index() {
@@ -31,6 +56,13 @@ export default function Index() {
 
         <section>
           <p>Boss of the day: {JSON.stringify(data.boss, null, 2)}</p>
+        </section>
+
+        <section className="text-center">
+          <p>Guesses: {data.guesses}</p>
+          <Form method="post">
+            <button>Guess</button>
+          </Form>
         </section>
 
         <section className="text-center">
